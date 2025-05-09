4d4ee0b12e13e468afc24e28f1936c65d17a8e21
BRUNO GIBRAN REYES ALVAREZ <al205757@alumnos.uacj.mx>
2025-05-09 10:34:34 -0600

    [INICIA] el proyecto final de pokemon

---
+++ Proyecto_Final_iOS/Proyecto_Final_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Proyecto_Final_iOS
+//
+//  Created by alumno on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Proyecto_Final_iOS/Proyecto_Final_iOS/Proyecto_Final_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_Final_iOSApp.swift
+//  Proyecto_Final_iOS
+//
+//  Created by alumno on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_Final_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
